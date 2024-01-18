bbe31cd710e4475c114408092fbd2fb1d966fa04
anastasiadergaeva <adergaeva@gmail.com>
2024-01-18 21:15:53 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ const redirectToDir = ({ root }) => ({
 });
 
 export default {
-  base: "/online-store-project",
+  base: "/online-store-project/",
   root: "./src",
   plugins: [redirectToDir({ root })],
   build: {
